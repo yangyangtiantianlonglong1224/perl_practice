18d2d13246d4dcae2d1002fda47f161b1e5cfa13
fhope <jingru951@163.com>
2020-08-06 22:05:24 +0800

    V字符串练习

---
+++ 3.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl5.16.3  -w
+
+use strict;
+use warnings;
+
+my $smile = v9786;
+my $foo = v102.111.111;
+my $martin = v77.97.114.116.105.110;
+
+print "smile = $smile\n";
+print "foo = $foo\n";
+print "martin = $martin\n";
